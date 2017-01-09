2b4485ef237653bd8d5c5f29a4ddc965f288ec29
Tanuj Srivastava <srim0987@gmail.com>
2017-01-09 17:53:39 +0530

    user articles assoc

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109095336) do
+ActiveRecord::Schema.define(version: 20170109103553) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
